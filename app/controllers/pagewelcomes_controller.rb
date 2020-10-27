85c664c1b105874d2d1b9b5a0db3ffbe7d8d4d70
todplaz <balocm@yahoo.fr>
2020-10-27 01:11:00 +0100

    https://github.com/todplaz/the_gossip_project

--- app/controllers/pagewelcomes_controller.rb
+++ app/controllers/pagewelcomes_controller.rb
@@ -1,4 +1,5 @@
 class PagewelcomesController < ApplicationController
 	def welcome
+		@user = params[:user]
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-	get '/team', to: 'pageteams#team'
-	get '/contact', to: 'pagecontacts#contact'
-	get '/welcome', to: 'pagewelcomes#welcome'
-	root to: 'pageacceuils#acceuil'
+	get '/team', to: 'pageteams#team',as:'team'
+	get '/contact', to: 'pagecontacts#contact',as:'contact'
+	
+	root to: 'pageacceuils#acceuil',as:'acceuil'
+	get '/welcome(/:user)', to: 'pagewelcomes#welcome', as:'welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
